2c13a10efa089fcb5cd38a79204e8233560ab381
hungmi <hungmihungmi@gmail.com>
2018-08-23 21:32:48 +0800

    logo 跟 menu 要 fixed
    footer 要遮住 contact btn, 可以被遮住一半
    
    以大標題上端為基準，把see more往上移動
    
    首頁大圖的總類標題背景要加白色

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,7 +1,7 @@
 $(document).on("click", ".toggler", function() {
 	var body = document.querySelector("body")
 	body.classList.toggle("active")
-	if (body.classList.contains("active")) {
-		window.scrollTo(0, 0)
-	}
+	// if (body.classList.contains("active")) {
+	// 	window.scrollTo(0, 0)
+	// }
 })
